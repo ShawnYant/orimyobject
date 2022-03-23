f70790c26ac7723ae1f215b09cba353546ffa239
ShawnYant <yanqiyu2002@gmail.com>
2022-03-23 16:46:20 -0400

    figuring login

--- Customer/apps.py
+++ Customer/apps.py
@@ -1,5 +1,5 @@
-from django.apps import AppConfig
+# from django.apps import AppConfig
 
 
-class CustomerConfig(AppConfig):
-    name = 'Customer'
+# class CustomerConfig(AppConfig):
+#     name = 'Customer'
--- Customer/urls.py
+++ Customer/urls.py
@@ -1,16 +1,16 @@
-#The child route of the web management system
-from django.urls import path,include
-from Customer import Cviews
-from web.views import index,cart
-from web.views import orders
-from web.views import orderhome
+# #The child route of the web management system
+# from django.urls import path,include
+# from Customer import Cviews
+# from web.views import index,cart
+# from web.views import orders
+# from web.views import orderhome
 
-# urlpatterns = [
-#     path('', Cviews.login,name="web_index"),
-#     path('order', orderhome.index,name="orders_homepage"),
+# # urlpatterns = [
+# #     path('', Cviews.login,name="web_index"),
+# #     path('order', orderhome.index,name="orders_homepage"),
 
-#     #The routes of The front-end  logs in /out
-#      path('regi', Cviews.register, name='web_regis'),
-#      path('logout', Cviews.logout),
-# ]
+# #     #The routes of The front-end  logs in /out
+# #      path('regi', Cviews.register, name='web_regis'),
+# #      path('logout', Cviews.logout),
+# # ]
 
